8233e899d80fe656300334e951f0b8a531acba3a
Ioana Leontiuc <i.leontiuc@student.tudelft.nl>
2017-03-21 17:13:17 +0100

    Update SecondBank.java

--- src/main/java/group/artifact/SecondBank.java
+++ src/main/java/group/artifact/SecondBank.java
@@ -2,6 +2,21 @@ package group.artifact;
 
 public class SecondBank {
 
+	private double balance;
+	private boolean isOverdraft = false;
+	
+	public boolean deposit(double amount) {
+		if (amount >= 0.00) { 
+			balance = balance + amount;
+			if(balance > 0.00){
+				isOverdraft = false;
+			}
+			return true; 
+		}else{
+			System.out.println("amount cannot be negative"); 
+			return false;
+		} 
+	} 
 	
 	
 	
